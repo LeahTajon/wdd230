f1a3657d52a1b341e46be56128121a343bcf0903
Leah Tajon <leahtajon@gmail.com>
2023-01-06 12:17:44 +1000

    Create course activity portal

---
+++ scripts/getDates.js
@@ -0,0 +1,5 @@
+// Get year
+document.getElementById('year').innerHTML = new Date().getFullYear();
+
+// Last modified
+document.getElementById('lastModified').innerHTML = `Last modified: ${document.lastModified}`;
