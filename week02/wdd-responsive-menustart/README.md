769cbf9b219dced26a9eafca5ce12e5f2baaf1e4
Leah Tajon <leahtajon@gmail.com>
2023-01-10 15:02:25 +1000

    Add responsive menustart

---
+++ week02/wdd-responsive-menustart/README.md
@@ -0,0 +1,5 @@
+# 🔒WDD Responsive Menu - Start
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/leahtajon/pen/BaPWpBo](https://codepen.io/leahtajon/pen/BaPWpBo).
+
+This is the second part of vertical icon nav, 
---
+++ week02/wdd-responsive-menustart/src/script.js
@@ -0,0 +1,8 @@
+const menubutton = document.querySelector("#menu-button");
+const menuitems = document.querySelectorAll(".menu-item");
+// menuitems will be a Node List of the list items
+
+menubutton.addEventListener("_____", () => {
+	menuitems.forEach((item) => item.classList.toggle("_____"));
+	menubutton.classList.toggle("_____");
+});
