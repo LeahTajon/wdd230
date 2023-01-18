6a29ad7205875e83b60010d2783728eccea2af42
Leah Tajon <leahtajon@gmail.com>
2023-01-19 09:26:11 +1000

    Add Web Storage API project

---
+++ week03/scripts/bom.js
@@ -0,0 +1,72 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+let chaptersArray = getChapterList() || [];
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+
+function displayList(item) {
+    let li = document.createElement('li');
+    let deletebutton = document.createElement('button');
+    li.textContent = item; // note the use of the item parameter and then delete this comment
+    deletebutton.textContent = '❌';
+    deletebutton.classList.add('delete'); // this references the CSS rule .delete{width:fit-content;} to size the delete button
+    li.append(deletebutton);
+    list.append(li);
+    deletebutton.addEventListener('click', function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent); // note this new function that is needed to remove the chapter from the array and localStorage.
+        input.focus();
+    });
+}
+
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+}
+
+button.addEventListener('click', () => {
+    // if (input.value != '') {
+    //     const chapter = input.value;
+
+    //     const li = document.createElement('li');
+    //     const deleteButton = document.createElement('button');
+
+    //     li.textContent = chapter;
+    //     deleteButton.textContent = '❌';
+
+    //     li.appendChild(deleteButton);
+    //     list.appendChild(li);
+
+    //     deleteButton.addEventListener('click', () => {
+    //         list.removeChild(li);
+    //         input.focus();
+    //     });
+
+    // } else {
+    //     input.focus();
+    // }
+    
+    // input.focus();
+    // input.value = '';
+
+    if (input.value != '') {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
+        input.value = '';
+        input.focus();
+    }
+});
