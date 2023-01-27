4cad311a0a4acff4975ae41be7b42a0dfa79581d
Leah Tajon <leahtajon@gmail.com>
2023-01-27 15:28:18 +1000

    Add contact form

--- scripts/menu-button.js
+++ scripts/menu-button.js
@@ -10,3 +10,29 @@ hamburger.addEventListener("click", () => {
         navMenu.classList.remove("active");
       }))
   })
+
+// Check PASSWORD 
+const password1 = document.querySelector('.password1');
+const password2 = document.querySelector('.password2');
+const message = document.querySelector('.form-message');
+
+function checkSame() {
+    if (password1.value !== password2.value) {
+        message.textContent = 'Password does not match!';
+        message.style.display = "block";
+		password2.style.backgroundColor = "#aa3333";
+		password2.style.color = "#fff";
+		password2.focus();
+		password2.value = "";
+    } else {
+        message.style.display = 'none';
+        password2.style.backgroundColor = "#fff";
+		password2.style.color = "#000";
+    }
+}
+
+password2.addEventListener('focusout', checkSame);
+
+function showValue(value) {
+    document.getElementById("rating-value").innerHTML = value;
+  }
