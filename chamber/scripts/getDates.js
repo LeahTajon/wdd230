1971a2f8165dedd920658f7c63021915de3d65a6
Leah Tajon <leahtajon@gmail.com>
2023-01-28 15:14:45 +1000

    Add join page

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -3,3 +3,10 @@ document.getElementById('year').innerHTML = new Date().getFullYear();
 
 // Last modified
 document.getElementById('lastModified').innerHTML = `Last modified: ${document.lastModified}`;
+
+
+window.onload = function() {
+    var currentDate = new Date();
+    document.getElementById("form-loaded-time").value = currentDate.toString();
+  }
+  
